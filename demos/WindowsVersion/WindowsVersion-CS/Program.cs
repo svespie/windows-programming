74d2c1acdeea7c34d98b61f06721bcfbca307db5
Stan Vespie <svespie@outlook.com>
2024-06-25 21:25:37 -0400

    added windows version stuff

---
+++ demos/WindowsVersion/WindowsVersion-CS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WindowsVersion_CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.OSVersion.VersionString);
+        }
+    }
+}
